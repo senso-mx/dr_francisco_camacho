c44188f9d5a7640cb1566fcdab7fdf18d94a00c5
Fidel Lopez <fideljclopez@gmail.com>
2024-05-20 22:25:18 +0200

    add prices

--- src/components/Surgeries.jsx
+++ src/components/Surgeries.jsx
@@ -1,33 +1,39 @@
 const SURGERIES = [
   {
     title: "Implante de mamas",
-    image: `${process.env.BASE_PATH || ''}/images/implantesdemama.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/implantesdemama.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Desde $70,000 MXN",
   },
   {
     title: "Liposucción",
-    image: `${process.env.BASE_PATH || ''}/images/lipo.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/lipo.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Desde $115,000 MXN",
   },
   {
     title: "Lifting Facial",
-    image: `${process.env.BASE_PATH || ''}/images/lifting.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/lifting.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Precio por Valoración",
   },
   {
     title: "Aumento de Glúteos",
-    image: `${process.env.BASE_PATH || ''}/images/gluteos.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/gluteos.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Desde $130,000 MXN",
   },
   {
     title: "Abdominoplastia",
-    image: `${process.env.BASE_PATH || ''}/images/abdomino.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/abdomino.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Desde $100,000 MXN",
   },
   {
     title: "Rinoplastia",
-    image: `${process.env.BASE_PATH || ''}/images/rino.webp`,
+    image: `${process.env.BASE_PATH || ""}/images/rino.webp`,
     link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+    price: "Desde $70,000 MXN",
   },
 ];
 
@@ -45,18 +51,23 @@ export default function Surgeries() {
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
               backgroundColor: "transparent",
-              aspectRatio : "16/9",
-              position: "relative"
+              aspectRatio: "16/9",
+              position: "relative",
             }}
           >
             <div className="absolute top-0 left-0 w-full h-full gradiente rounded-lg"></div>
-            <h3 className="text-center text-white z-10">{surgery.title}</h3>
+            <div className="z-10">
+              <h3 className="text-center text-white">{surgery.title}</h3>
+                  <hr />
+                  <p className="px-12 text-white">{surgery.price}</p>
+            </div>
+
             <a
               className="btn btn-secondary btn-sm px-12  z-10"
               href={surgery.link}
               target="_blank"
               rel="noopener noreferrer"
-              role="button" 
+              role="button"
             >
               Solicitar Valoración
             </a>
